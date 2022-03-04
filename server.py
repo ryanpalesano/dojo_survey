ff0462647f98cfbc64c4527a335705fefce6a454
Ryan Palesano <89495764+ryanpalesano@users.noreply.github.com>
2022-03-04 17:29:35 -0600

    Add files via upload

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, request, session
+app = Flask(__name__)
+app.secret_key = "secrets"
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['languages'] = request.form['languages']
+    session['comment'] = request.form['comment']
+    return redirect('/success')
+
+
+@app.route('/success')
+def success():
+    return render_template("result.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
